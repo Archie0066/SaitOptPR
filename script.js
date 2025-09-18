76db9bfdff32d31838057a50059239129221442f
Archie0066 <archie.active2007@gmail.com>
2025-09-18 22:18:59 +0600

    3424

---
+++ script.js
@@ -0,0 +1,64 @@
+const sheetUrls = [
+  'https://docs.google.com/spreadsheets/d/16qzBLyi0hvPna5YPZxIqfBi_HYl4ezUarPLR97k6RWQ/edit?usp=sharing', // моя таблица 
+  'https://docs.google.com/spreadsheets/d/1XiKE2moWTC0ulSzEyTKD5sKUIaeTBGPrCH3yOXgHk4M/edit?usp=sharing' // таблица Деревяги
+];
+
+let currentIndex = 0;
+let slides = [];
+
+function loadAllSheets() {
+  const container = document.getElementById('slider-content');
+  container.innerHTML = '';
+  slides = [];
+
+  for (let i = 0; i < sheetUrls.length; i++) {
+    const slide = document.createElement('div');
+    slide.classList.add('slide');
+
+    const iframe = document.createElement('iframe');
+    iframe.src = sheetUrls[i] + '&t=' + Date.now();  
+    iframe.width = '100%';
+    iframe.height = '6000px';
+    iframe.frameBorder = '0';
+    iframe.allowFullscreen = true;
+    iframe.onload = () => console.log('Iframe loaded for slide ' + i);  
+    iframe.onerror = () => {
+      console.error('Iframe failed to load for slide ' + i);
+      
+      slide.innerHTML = '<p>Не удалось загрузить файл. <a href="' + sheetUrls[i] + '" target="_blank">Открыть в новой вкладке</a></p>';
+    };
+
+    slide.appendChild(iframe);
+    container.appendChild(slide);
+    slides.push(slide);
+  }
+
+  if (slides.length > 0) {
+    showSlide(0);
+  }
+}
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+  currentIndex = index;
+}
+
+
+document.querySelector('.prev').addEventListener('click', () => {
+  if (slides.length > 0) {
+    showSlide((currentIndex - 1 + slides.length) % slides.length);
+  }
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  if (slides.length > 0) {
+    showSlide((currentIndex + 1) % slides.length);
+  }
+});
+
+loadAllSheets();
