95468a5f75232b4b0d559e78d122a6c3986ccb8d
Archie0066 <archie.active2007@gmail.com>
2025-09-21 21:35:40 +0600

    ааа

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const sheetUrls = [
-'https://docs.google.com/spreadsheets/d/e/2PACX-1vSUXau2tPe1749501W_g_CRNePsz9snloT7royNPt8L441VzhtCEHfCgcUZxa3bWBNDYb10ctatF6si/pub?output=csv'
+'https://docs.google.com/spreadsheets/d/e/2PACX-1vTBc6F7PeogNf_R0i_CvNAuKsDAugTAej-etqTmKqg9CU2u16f7DIDYhSvf4hRTmAHqXttKvP9C_7Re/pub?output=tsv'
+  // 2-й слайд: лидер дня
 
 ];
 
@@ -16,8 +17,10 @@ async function loadAllSheets() {
     let content;
 
     if (i === 0) {
+      // первый слайд = таблица
       content = renderTable(data);
     } else {
+      // второй слайд = карточка лидера дня
       content = renderLeaderCard(data);
     }
 
@@ -62,14 +65,17 @@ function renderTable(data) {
 }
 
 function renderLeaderCard(data) {
+  // предполагаем, что заголовки в первой строке
   const headers = data[0];
   const rows = data.slice(1);
 
+  // ищем колонку "Очки" (или "Score")
   const scoreIndex = headers.findIndex(h => /очк|score/i.test(h));
   if (scoreIndex === -1) {
     return document.createTextNode("Не найдена колонка 'Очки'");
   }
 
+  // находим лидера
   let leader = rows[0];
   let maxScore = parseFloat(rows[0][scoreIndex]) || 0;
 
@@ -81,6 +87,7 @@ function renderLeaderCard(data) {
     }
   }
 
+  // создаём карточку
   const card = document.createElement('div');
   card.classList.add('leader-card');
 
